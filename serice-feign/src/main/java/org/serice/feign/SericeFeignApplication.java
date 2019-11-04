a1ed5509f5c20aa45e9f763171ad6935e07c03f5
林浩东 <linhd2@centaline.com.cn>
2019-11-04 17:11:10 +0800

    init

---
+++ serice-feign/src/main/java/org/serice/feign/SericeFeignApplication.java
@@ -0,0 +1,18 @@
+package org.serice.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+/*在程序的启动类ServiceFeignApplication ，加上@EnableFeignClients注解开启Feign的功能：*/
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class SericeFeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SericeFeignApplication.class, args);
+	}
+
+}
