db67c7853654689b3af7be873acdc20fffd41943
林浩东 <linhd2@centaline.com.cn>
2019-11-05 09:53:08 +0800

    git命令文件添加

--- config-client/src/main/java/org/config/client/App.java
+++ config-client/src/main/java/org/config/client/App.java
@@ -12,7 +12,7 @@ public class App{
     public static void main( String[] args ){
         SpringApplication.run(App.class, args);
     }
-    @Value("${foo}")
+    @Value("${management.security.enabled}")
 	String foo;
 	@RequestMapping(value = "/hi")
 	public String hi(){
