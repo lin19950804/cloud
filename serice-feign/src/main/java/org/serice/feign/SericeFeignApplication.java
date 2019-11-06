5257e993039c37d8ff75bc3a15db08465d127bdd
林浩东 <linhd2@centaline.com.cn>
2019-11-06 16:15:04 +0800

    添加dockerfile

--- serice-feign/src/main/java/org/serice/feign/SericeFeignApplication.java
+++ serice-feign/src/main/java/org/serice/feign/SericeFeignApplication.java
@@ -3,7 +3,8 @@ package org.serice.feign;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 /*在程序的启动类ServiceFeignApplication ，加上@EnableFeignClients注解开启Feign的功能：*/
 @SpringBootApplication
